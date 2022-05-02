77838e6803843306d8003a15f463b02a153113d5
matheus1810 <matheussouza1810@gmail.com>
2022-05-01 21:03:52 -0300

    modal

--- src/components/card/CardProduct.jsx
+++ src/components/card/CardProduct.jsx
@@ -6,37 +6,35 @@ import NestedModal from '../modal/NestedModal';
 export const CardProduct = () => {
 
     const [modalIsVisible, setmodalIsVisible] = useState(false)
-    
+
     return (
+        <>
         <StyleCard
-        onClick={() => {
-            setmodalIsVisible(true)      
-            console.log('ss')  
-          }}
-        style={{ width: '18rem' }}>
-           {modalIsVisible && <NestedModal
-           modalIsVisible={modalIsVisible} setmodalIsVisible={setmodalIsVisible}
-           >
-               <button
-                 onClick={() => {
-                    setmodalIsVisible(false) 
-                    console.log(modalIsVisible)       
-                    console.log('fechar')  
-                  }}
-               >fechar</button>
-           </NestedModal>
-           
-           }
+            onClick={() => {
+                setmodalIsVisible(true)
+                console.log('')
+            }}
+            style={{ width: '18rem' }}>
+
             <StyleCardGroup>
                 <StyleCard.Img variant="top" src="https://picsum.photos/100/100" />
-                <StyleCard.Body style={{display: 'flex', flexDirection: 'column', alignItems:'center'}}>
-                    <StyleCard.Title style={{fontFamily:'Montserrat',fontSize: '18px',color: '#000000'}}>Natura</StyleCard.Title>
-                    <StyleCard.Text style={{color: '#12995A',fontSize: '12px',letterSpacing: '-0.01em' }}>
+                <StyleCard.Body style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                    <StyleCard.Title style={{ fontFamily: 'Montserrat', fontSize: '18px', color: '#000000' }}>Natura</StyleCard.Title>
+                    <StyleCard.Text style={{ color: '#12995A', fontSize: '12px', letterSpacing: '-0.01em' }}>
                         Você receberá 3,5% de CashBack.
                     </StyleCard.Text>
                 </StyleCard.Body>
             </StyleCardGroup>
-   </StyleCard>
+        </StyleCard>
+        {modalIsVisible &&
+                <NestedModal
+                    modalIsVisible={modalIsVisible}
+                    setmodalIsVisible={setmodalIsVisible}
+                >
+                </NestedModal>
+            }
+            </>
+        
     )
 }
 
--- src/components/modal/NestedModal.js
+++ src/components/modal/NestedModal.js
@@ -2,6 +2,8 @@ import * as React from 'react';
 import Box from '@mui/material/Box';
 import Modal from '@mui/material/Modal';
 import Button from '@mui/material/Button';
+import { ChildModalContainer, ComoFuciona, ImgContainer, InfosGrid, ModalContainer } from './styled';
+import natura from "../../assets/logo/natura.png"
 
 const style = {
   position: 'absolute',
@@ -15,6 +17,8 @@ const style = {
   pt: 2,
   px: 4,
   pb: 3,
+  width: '70vw',
+  height: '90vh'
 };
 
 function ChildModal() {
@@ -28,7 +32,9 @@ function ChildModal() {
 
   return (
     <React.Fragment>
-      <Button onClick={handleOpen}>Open Child Modal</Button>
+      <Button
+        style={{ backgroundColor: '#10BA6B', color: 'white', borderRadius: '5px', marginTop: '50px' }}
+        onClick={handleOpen}>Ativar cashback e ir para a loja</Button>
       <Modal
         hideBackdrop
         open={open}
@@ -36,34 +42,101 @@ function ChildModal() {
         aria-labelledby="child-modal-title"
         aria-describedby="child-modal-description"
       >
-        <Box sx={{ ...style, width: 200 }}>
-          <h2 id="child-modal-title">Text in a child modal</h2>
-          <p id="child-modal-description">
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-          </p>
-          <Button onClick={handleClose}>Close Child Modal</Button>
+        <Box sx={{ ...style, }}>
+
+          <ChildModalContainer>
+
+            <div>
+
+              <ComoFuciona>
+                <h4>Como funciona?</h4>
+                <p>Veja como é simples ajudar quem precisa!</p>
+              </ComoFuciona>
+
+              <InfosGrid>
+                <div>
+                  <div>icon</div>
+                  <h4>Ativar Cashback</h4>
+                  <p>Clique em Ativar cashback e ir para loja e seja direcionado
+                    para o site da loja participante.</p>
+                </div>
+                <div>
+                  <div>icon</div>
+                  <h4>Compre normalmente</h4>
+                  <p>Assim que você for direcionado para a loja você poderá
+                    comprar normalmente seus produtos favoritos com descontos incríveis.</p>
+                </div>
+                <div>
+                  <div>icon</div>
+                  <h4>Pronto!</h4>
+                  <p>Após finalizar a compra, a loja nos avisará sobre o valor disponibilizado
+                    e ele aparecerá dentro de alguns dias na sua Dashboard!</p>
+                </div>
+              </InfosGrid>
+            </div>
+            <h4>Regras e Exeções</h4>
+            <div>
+              <p>A empresa escolhida tem até 2 dia(s)
+                para nos avisar da compra e o dinheiro aparecer no seu
+                saldo como pendente (será confirmado em até 20 dias).</p>
+
+              <p>Cashback válido apenas para compras online.</p>
+              <p></p>
+
+            </div>
+            <div>
+
+            </div>
+          </ChildModalContainer>
+
+          <Button onClick={handleClose}>Voltar</Button>
         </Box>
       </Modal>
     </React.Fragment>
   );
 }
 
-export default function NestedModal({children,modalIsVisible,setmodalIsVisible}) {
+export default function NestedModal({ children, modalIsVisible, setmodalIsVisible }) {
   const handleOpen = () => setmodalIsVisible(true);
   const handleClose = () => setmodalIsVisible(false);
 
   return (
     <div>
-      <Button onClick={handleOpen}>Open modal</Button>
       <Modal
-      style={{ backdropFilter:'blur(10px)'}}
+        style={{ backdropFilter: 'blur(10px)' }}
         open={modalIsVisible}
         onClose={handleClose}
         aria-labelledby="parent-modal-title"
         aria-describedby="parent-modal-description"
       >
-        <Box sx={{ ...style, width: 400 }}>
-        {children}
+        <Box sx={{ ...style }}>
+
+          <ModalContainer>
+            <ImgContainer>
+              <img src={natura} />
+            </ImgContainer>
+            <div>
+              <h3>Natura</h3>
+              <div>
+                <div>5,2%de cashback para o meio ambiente</div>
+              </div>
+
+              <p>Como funciona o cashback</p>
+              <p>Receba cashback das suas compras online nas
+                lojas parceiras e contribua para causas sociais .
+                Ajudar o planeta começa com atitudes simples e práticas.</p>
+
+              <div>
+                <p>Ver regras do cashback</p>
+                <div>
+                  <input type="checkbox" />
+                  Li e concordo com os termos e condições</div>
+              </div>
+
+            </div>
+          </ModalContainer>
+
+          {children}
           <ChildModal />
         </Box>
       </Modal>
